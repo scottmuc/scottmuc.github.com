16b615703aa715f84739e838daf478f5a44f1750
Scott Muc <scott@scottmuc.com>
2024-01-22 11:07:46 +0100

    Add frontmatter parsing of legacy blog posts

--- migrate.go
+++ migrate.go
@@ -6,7 +6,43 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
+
+	"gopkg.in/yaml.v3"
 )
+
+type OctopressFrontMatter struct {
+	Title string
+	Date OctopressDateTime
+}
+
+type OctopressDateTime struct {
+	time.Time
+}
+
+// Dates in Octopress seem to be a mixture of the following date formats
+const octopressTimeFormat = "2006-01-02 15:04"
+const octopressTimeFormat2 = "2006-01-02 15:04:05 -0700"
+
+
+// Behold the ugly!
+func (odt *OctopressDateTime) UnmarshalYAML(value *yaml.Node) error {
+	strValue := value.Value
+	parsedTime, err := time.Parse(octopressTimeFormat, strValue)
+	if err != nil {
+		parsedTime, err = time.Parse(octopressTimeFormat2, strValue)
+		if err != nil {
+			return err
+		} else {
+			odt.Time = parsedTime
+		}
+
+	} else {
+		odt.Time = parsedTime
+	}
+
+	return nil
+}
 
 func MigratePost(path string, fileInfo os.FileInfo, err error) error {
 	if fileInfo.IsDir() {
@@ -16,8 +52,6 @@ func MigratePost(path string, fileInfo os.FileInfo, err error) error {
 	octopressFilename := filepath.Base(path)
 	hugoFilename := strings.Replace(octopressFilename[11:], ".markdown", ".md", -1)
 	hugoFilePath := "hugo/content/blog/" + hugoFilename
-	fmt.Printf("%s -> %s\n", path, hugoFilePath)
-
 
 	octopressFileContents, e := os.ReadFile(path)
 	if e != nil {
@@ -25,7 +59,17 @@ func MigratePost(path string, fileInfo os.FileInfo, err error) error {
 	}
 
 	frontMatter := strings.Split(string(octopressFileContents), "---")[1]
-	fmt.Println(frontMatter)
+
+	t := OctopressFrontMatter{}
+
+	e2 := yaml.Unmarshal([]byte(frontMatter), &t)
+	if e2 != nil {
+		log.Fatalf("error: %v", e2)
+	}
+
+	fmt.Printf("%s -> %s\n", path, hugoFilePath)
+	fmt.Printf("title: %s\n", t.Title)
+	fmt.Printf("date: %v\n", t.Date)
 
 	return nil
 }
