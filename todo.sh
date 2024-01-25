4885ce0308901b0f7d23069df4188239db171e28
Scott Muc <scott@scottmuc.com>
2024-01-25 16:16:37 +0100

    Add todo of migration stuff

---
+++ todo.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo Copy podcasts_opml.xml
+echo Copy twinery boat tour
+echo Fix RSS image in podcasts post
+echo Resolve permalinks if possible
+echo Sort of RSS feed
+echo Create Custom 404
+echo Update Robots.txt
+echo Create post page-bundle archetype
+echo Create Youtube shortcode and possible migrate
+echo Octopress directives to fix
+echo
+grep -R {% hugo/content
+
