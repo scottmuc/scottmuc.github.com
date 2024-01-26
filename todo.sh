027d905cd08dc86fa69237e5f586a83080698014
Scott Muc <scott@scottmuc.com>
2024-01-26 15:38:02 +0100

    Add custom 404

--- todo.sh
+++ todo.sh
@@ -4,6 +4,5 @@ echo Copy podcasts_opml.xml
 echo Migrate static pages, like the about page
 echo Copy twinery boat tour
 echo Sort out RSS feed
-echo Create Custom 404
 echo Create post page-bundle archetype
 echo Fix Twitter inline references
