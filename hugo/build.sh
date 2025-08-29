246b4fccfa1f9eebd68bfbcdcdcf9b50a9106e1a
Scott Muc <scott@scottmuc.com>
2025-08-29 22:58:36 +0200

    Added concourse pipeline and nixified things

--- hugo/build.sh
+++ hugo/build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 rm -rf public
-hugo --gc
+if command -v nix 2>/dev/null; then
+  nix --extra-experimental-features "nix-command flakes" run nixpkgs#hugo -- --gc
+else
+  hugo --gc
+fi
 rm -rf public/1
